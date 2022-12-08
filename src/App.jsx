6b03ba928f5cd0f5121919e797db8b85a41b90cb
MsTYsN <yassinemestaoui98@gmail.com>
2022-12-08 23:40:27 +0100

    Feat: Adding basic authentification view

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import * as React from "react";
+import * as React from 'react';
 
 // react tools imports
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // styling imports
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react';
 
 // views import
-import Home from "../src/views/home/Home";
-import UserSignUp from '../src/views/user-auth/UserSignUp'
-import UserSignIn from '../src/views/user-auth/UserSignIn'
+import Home from '../src/views/home/Home';
+import UserSignUp from '../src/views/user-auth/UserSignUp';
+import UserSignIn from '../src/views/user-auth/UserSignIn';
 
-// static files import 
-import theme from '../src/utils/styles/theme'
-import '@fontsource/black-han-sans/400.css'
-import '@fontsource/roboto-slab/500.css'
-import '@fontsource/open-sans/300.css'
+// static files import
+import theme from '../src/utils/styles/theme';
+import '@fontsource/black-han-sans/400.css';
+import '@fontsource/roboto-slab/500.css';
+import '@fontsource/open-sans/300.css';
 
 function App() {
   return (
@@ -24,13 +24,9 @@ function App() {
         <div className="App">
           <div className="content">
             <Routes>
-              <Route exact path="/" element={<Home />}/>
-            </Routes>
-            <Routes>
-              <Route exact path="/user-signin" element={<UserSignIn />}/>
-            </Routes>
-            <Routes>
-              <Route exact path="/user-signup" element={<UserSignUp />}/>
+              <Route exact path="/" element={<Home />}></Route>
+              <Route exact path="/user-signup" element={<UserSignUp />}></Route>
+              <Route exact path="/user-signin" element={<UserSignIn />}></Route>
             </Routes>
           </div>
         </div>
--- src/views/user-auth/UserSignUp.jsx
+++ src/views/user-auth/UserSignUp.jsx
@@ -1,37 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import * as Yup from "yup";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
 
 // toolbox imports
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import { Box, Flex } from "@chakra-ui/react";
-import { Heading, Button } from "@chakra-ui/react";
-
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Box, Flex } from '@chakra-ui/react';
+import { Heading, Button } from '@chakra-ui/react';
 
 // import static files
-import RegistrationBG from "../../assets/img/user_registration_bg.png";
+import RegistrationBG from '../../assets/img/user_registration_bg.png';
 
 const initialValues = {
-  email: "",
-  password: "",
-  cPassword: "",
-  fullName: "",
-  profile:{
-    owner: "",
-    player: ""
-  }
+  email: '',
+  password: '',
+  cPassword: '',
+  fullName: '',
+  profile: {
+    owner: '',
+    player: '',
+  },
 };
 
 const onSubmit = (values) => {
-  console.log("form data", values);
+  console.log('form data', values);
 };
 
 const validationSchema = Yup.object({
-  email: Yup.string().required("Required field"),
-  password: Yup.string().required("Required field"),
-  cPassword: Yup.string().required("Required field"),
-  fullName: Yup.string().required("Required field"),
-  profile: Yup.string().required("Required field"),
+  email: Yup.string().required('Required field'),
+  password: Yup.string().required('Required field'),
+  cPassword: Yup.string().required('Required field'),
+  fullName: Yup.string().required('Required field'),
+  profile: Yup.string().required('Required field'),
 });
 
 const UserSignUp = () => {
@@ -46,8 +45,8 @@ const UserSignUp = () => {
       />
       <Box bg="#080808" w="60%" p="8% 10%">
         <Heading variant="logo" as="h1" size="3xl">
-          <span style={{ color: "#19D2C2" }}>Foot</span>
-          <span style={{ color: "#0AADE8" }}>Planet</span>
+          <span style={{ color: '#19D2C2' }}>Foot</span>
+          <span style={{ color: '#0AADE8' }}>Planet</span>
         </Heading>
         <Heading variant="welcomeMessage" as="h3">
           Get started
@@ -56,7 +55,7 @@ const UserSignUp = () => {
           already have an account ?
           <Link
             to="/user-signin"
-            style={{ color: "#0AADE8", margin: "0 0 0 1%" }}
+            style={{ color: '#0AADE8', margin: '0 0 0 1%' }}
           >
             sign in
           </Link>
@@ -64,58 +63,146 @@ const UserSignUp = () => {
 
         {/* Registration form */}
         <Formik
-         initialValues={initialValues}
-         onSubmit={onSubmit}
-         validationSchema={validationSchema}>
-          <Form style={{margin: "2% 0"}}>
-            <label htmlFor="email" style={{color: "#D9D9D9", fontSize: "0.85rem"}}>Email</label>
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={validationSchema}
+        >
+          <Form style={{ margin: '2% 0' }}>
+            <label
+              htmlFor="email"
+              style={{ color: '#D9D9D9', fontSize: '0.85rem' }}
+            >
+              Email
+            </label>
             <Field
               type="email"
               id="email"
               name="email"
               placeholder="Enter your email address"
-              style={{width: "100%", backgroundColor: "#1A1D22", height: "2.5rem", border:"1px solid #12323d", fontSize: "0.85rem", padding: "0 2%", color: "#F5F5F5"}}
+              style={{
+                width: '100%',
+                backgroundColor: '#1A1D22',
+                height: '2.5rem',
+                border: '1px solid #12323d',
+                fontSize: '0.85rem',
+                padding: '0 2%',
+                color: '#F5F5F5',
+              }}
             ></Field>
-            <ErrorMessage name="email" component="small" style={{color: "#19D2C2"}} />
+            <ErrorMessage
+              name="email"
+              component="small"
+              style={{ color: '#19D2C2' }}
+            />
             <br />
-            <label htmlFor="password" style={{color: "#D9D9D9", fontSize: "0.85rem"}}>Password</label>
+            <label
+              htmlFor="password"
+              style={{ color: '#D9D9D9', fontSize: '0.85rem' }}
+            >
+              Password
+            </label>
             <Field
               type="password"
               id="password"
               name="password"
               placeholder="Enter your password"
-              style={{width: "100%", backgroundColor: "#1A1D22", height: "2.5rem", border:"1px solid #12323d", fontSize: "0.85rem", padding: "0 2%", color: "#F5F5F5"}}
+              style={{
+                width: '100%',
+                backgroundColor: '#1A1D22',
+                height: '2.5rem',
+                border: '1px solid #12323d',
+                fontSize: '0.85rem',
+                padding: '0 2%',
+                color: '#F5F5F5',
+              }}
             ></Field>
-            <ErrorMessage name="password" component="small" style={{color: "#19D2C2"}} />
+            <ErrorMessage
+              name="password"
+              component="small"
+              style={{ color: '#19D2C2' }}
+            />
             <br />
-            <label htmlFor="cPassword" style={{color: "#D9D9D9", fontSize: "0.85rem"}}>Confirm Password</label>
+            <label
+              htmlFor="cPassword"
+              style={{ color: '#D9D9D9', fontSize: '0.85rem' }}
+            >
+              Confirm Password
+            </label>
             <Field
               type="password"
               id="password"
               name="cPassword"
               placeholder="Confirm your password"
-              style={{width: "100%", backgroundColor: "#1A1D22", height: "2.5rem", border:"1px solid #12323d", fontSize: "0.85rem", padding: "0 2%", color: "#F5F5F5"}}
+              style={{
+                width: '100%',
+                backgroundColor: '#1A1D22',
+                height: '2.5rem',
+                border: '1px solid #12323d',
+                fontSize: '0.85rem',
+                padding: '0 2%',
+                color: '#F5F5F5',
+              }}
             ></Field>
-            <ErrorMessage name="cPassword" component="small" style={{color: "#19D2C2"}} />
+            <ErrorMessage
+              name="cPassword"
+              component="small"
+              style={{ color: '#19D2C2' }}
+            />
             <br />
-            <label htmlFor="fullName" style={{color: "#D9D9D9", fontSize: "0.85rem"}}>Full name</label>
+            <label
+              htmlFor="fullName"
+              style={{ color: '#D9D9D9', fontSize: '0.85rem' }}
+            >
+              Full name
+            </label>
             <Field
               type="text"
               id="fullName"
               name="fullName"
               placeholder="Enter your Enter your full name"
-              style={{width: "100%", backgroundColor: "#1A1D22", height: "2.5rem", border:"1px solid #12323d", fontSize: "0.85rem", padding: "0 2%", color: "#F5F5F5"}}
+              style={{
+                width: '100%',
+                backgroundColor: '#1A1D22',
+                height: '2.5rem',
+                border: '1px solid #12323d',
+                fontSize: '0.85rem',
+                padding: '0 2%',
+                color: '#F5F5F5',
+              }}
             ></Field>
-            <ErrorMessage name="fullName" component="small" style={{color: "#19D2C2"}} />
+            <ErrorMessage
+              name="fullName"
+              component="small"
+              style={{ color: '#19D2C2' }}
+            />
             <br />
-            <label htmlFor="profile" style={{color: "#D9D9D9", fontSize: "0.85rem"}}>You are a...</label>
-            <select id="profile" name="profile" style={{width: "100%", backgroundColor: "#1A1D22", height: "2.5rem", border:"1px solid #12323d", fontSize: "0.85rem", padding: "0 2%", color: "#F5F5F5"}}>
+            <label
+              htmlFor="profile"
+              style={{ color: '#D9D9D9', fontSize: '0.85rem' }}
+            >
+              You are a...
+            </label>
+            <select
+              id="profile"
+              name="profile"
+              style={{
+                width: '100%',
+                backgroundColor: '#1A1D22',
+                height: '2.5rem',
+                border: '1px solid #12323d',
+                fontSize: '0.85rem',
+                padding: '0 2%',
+                color: '#F5F5F5',
+              }}
+            >
               <option value="1">Player</option>
               <option value="2">Owner</option>
             </select>
             <br />
             <br />
-            <Button type="submit" w="100%" bg="#0AADE8">Sign up</Button>
+            <Button type="submit" w="100%" bg="#0AADE8">
+              Sign up
+            </Button>
           </Form>
         </Formik>
       </Box>
