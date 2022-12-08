7ce23ba5fe0c9beba7760d359be591b09d61cbdc
MsTYsN <yassinemestaoui98@gmail.com>
2022-12-08 23:40:27 +0100

    Feat: Adding basic authentification view

--- src/views/user-auth/UserSignIn.jsx
+++ src/views/user-auth/UserSignIn.jsx
@@ -1,10 +1,123 @@
-import React from 'react'
+import React from 'react';
+import { Box, Heading, Button, ButtonGroup } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
+import UserSignInImage from '../../assets/img/usersignin.png';
+
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const initialValues = {
+  email: '',
+  password: '',
+};
+
+const onSubmit = (values) => {
+  console.log('form data', values);
+};
+
+const validationSchema = Yup.object({
+  email: Yup.string().required('Required field'),
+  password: Yup.string().required('Required field'),
+});
 
 const UserSignIn = () => {
   return (
-   <div>Sign in</div>
-  )
-}
+    <Box display="flex">
+      <Box
+        bgImage={UserSignInImage}
+        w="40%"
+        h="100vh"
+        bgPosition="bottom"
+        bgSize="100%"
+      />
+      <Box bg="#000000" w="60%" p="8% 10%">
+        <Heading variant="logo" as="h1" size="3xl" noOfLines={1}>
+          <span style={{ color: '#19D2C2' }}>Foot</span>
+          <span style={{ color: '#0AADE8' }}>Planet</span>
+        </Heading>
+        <Heading variant="welcomeMessage" as="h3" noOfLines={1}>
+          <span>Welcome back</span>
+        </Heading>
+        <Heading variant="helperMessage" as="h6" size="m" noOfLines={1}>
+          <span>don't have an account ? </span>
+          <Link
+            to="/user-signup"
+            style={{ color: '#0AADE8', margin: '0 0 0 1%' }}
+          >
+            sign up
+          </Link>
+        </Heading>
 
-export default UserSignIn
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={validationSchema}
+        >
+          <Form style={{ margin: '3% 0' }}>
+            <label
+              htmlFor="Email"
+              style={{ color: '#D9D9D9', fontSize: '0.85rem' }}
+            >
+              Email
+            </label>
+            <Field
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email address"
+              style={{
+                width: '100%',
+                backgroundColor: '#1A1D22',
+                height: '2.5rem',
+                border: '1px solid #12323d',
+                fontSize: '0.85rem',
+                padding: '0 2%',
+                color: '#F5F5F5',
+              }}
+            ></Field>
+            <ErrorMessage
+              name="email"
+              component="small"
+              style={{ color: '#19D2C2' }}
+            />
+            <br />
+            <label
+              htmlFor="Password"
+              style={{ color: '#D9D9D9', fontSize: '0.85rem' }}
+            >
+              Password
+            </label>
+            <Field
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              style={{
+                width: '100%',
+                backgroundColor: '#1A1D22',
+                height: '2.5rem',
+                border: '1px solid #12323d',
+                fontSize: '0.85rem',
+                padding: '0 2%',
+                color: '#F5F5F5',
+              }}
+            ></Field>
+            <ErrorMessage
+              name="password"
+              component="small"
+              style={{ color: '#19D2C2' }}
+            />
+            <br />
+            <br />
+            <Button type="submit" w="100%" bg="#0AADE8">
+              Sign in
+            </Button>
+          </Form>
+        </Formik>
+      </Box>
+    </Box>
+  );
+};
+
+export default UserSignIn;
