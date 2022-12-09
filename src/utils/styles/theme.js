842f59ba9720aef71f9257c03f9562f930455ba3
MsTYsN <yassinemestaoui98@gmail.com>
2022-12-09 01:58:48 +0100

    Style: User Sign In styling

--- src/utils/styles/theme.js
+++ src/utils/styles/theme.js
@@ -1,5 +1,5 @@
-import { extendTheme } from "@chakra-ui/react";
-import HeadingTheme from "./HeadingTheme";
+import { extendTheme } from '@chakra-ui/react';
+import HeadingTheme from './HeadingTheme';
 
 const theme = extendTheme({
   components: { Heading: HeadingTheme },
