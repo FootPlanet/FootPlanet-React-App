4671aa59bd120f67834b0b9b66033cdb11258328
MsTYsN <yassinemestaoui98@gmail.com>
2022-12-09 01:58:48 +0100

    Style: User Sign In styling

--- src/utils/styles/HeadingTheme.js
+++ src/utils/styles/HeadingTheme.js
@@ -1,27 +1,27 @@
-import { defineStyle, defineStyleConfig } from '@chakra-ui/react'
+import { defineStyle, defineStyleConfig } from '@chakra-ui/react';
 
 const logo = defineStyle({
-    fontFamily: "Black Han Sans",
-})
+  fontFamily: 'Black Han Sans',
+});
 
 const welcomeMessage = defineStyle({
-    fontFamily: "Roboto Slab",
-    color: '#F5F5F5',
-    margin: '10% 0 4%',
-    fontSize: '1.8rem'
-})
+  fontFamily: 'Roboto Slab',
+  color: '#F5F5F5',
+  margin: '10% 0 4%',
+  fontSize: '1.8rem',
+});
 
 const helperMessage = defineStyle({
-    fontFamily: "Open Sans",
-    color: '#D6D6D6'
-})
+  fontFamily: 'Open Sans',
+  color: '#D6D6D6',
+});
 
 const HeadingTheme = defineStyleConfig({
-    variants: {
-        "logo": logo,
-        "welcomeMessage": welcomeMessage,
-        "helperMessage": helperMessage,
-    },
-})
- 
+  variants: {
+    logo: logo,
+    welcomeMessage: welcomeMessage,
+    helperMessage: helperMessage,
+  },
+});
+
 export default HeadingTheme;
