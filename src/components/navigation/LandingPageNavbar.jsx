b23c248248d669c1388592cb3799af5871bcd402
k1ssa1 <samikissai.edu@gmail.com>
2022-12-25 15:59:22 +0100

    Fix: fixing navlinks redirection on landingpage navbar

--- src/components/navigation/LandingPageNavbar.jsx
+++ src/components/navigation/LandingPageNavbar.jsx
@@ -9,7 +9,7 @@ const LandingPageNavbar = () => {
     <Grid templateColumns="repeat(3, 1fr)" gap={6} padding="1% 2%">
       <Flex>
         <Center>
-          <Link href="/">
+          <Link to="/">
             <Heading variant="logo" as="h1" size="2xl">
               <span style={{ color: "#19D2C2" }}>Foot</span>
               <span style={{ color: "#0AADE8" }}>Planet</span>
@@ -19,21 +19,21 @@ const LandingPageNavbar = () => {
       </Flex>
       <Flex justifyContent="space-evenly">
         <Center>
-          <Link href="#intro">
+          <Link to="#intro">
             <Heading variant="navLink" as="h6" size="l">
               Home
             </Heading>
           </Link>
         </Center>
         <Center>
-          <Link href="#services">
+          <Link to="#services">
             <Heading variant="navLink" as="h6" size="l">
               Services
             </Heading>
           </Link>
         </Center>
         <Center>
-          <Link href="#services">
+          <Link to="#services">
             <Heading variant="navLink" as="h6" size="l">
               Our values
             </Heading>
@@ -42,12 +42,12 @@ const LandingPageNavbar = () => {
       </Flex>
       <Flex>
         <Center>
-          <Link href="/user-signin">
+          <Link to="/user-signin">
             <Box as="button" fontSize="22px" padding="5% 10%" borderRadius="10%" width="5rem" color='#0AADE8' _hover={{ bg: '#ebedf0' }}>
               Log in
             </Box>
           </Link>
-          <Link href="/user-signin">
+          <Link to="/user-signup">
             <Box as="button" fontSize="22px" padding="5% 10%" borderRadius="2%" bg="#0AADE8" width="10rem" color='#F5F5F5' _hover={{ bg: '#00506d' }}>
               Get started
             </Box>
