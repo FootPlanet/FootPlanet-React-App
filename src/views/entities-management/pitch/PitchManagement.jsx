dd56b7d92fc1fca15c6828cc9f144510b60848fb
k1ssa1 <samikissai.edu@gmail.com>
2022-12-25 19:05:03 +0100

    Feat: adding styled pitch management toolbar component

--- src/views/entities-management/pitch/PitchManagement.jsx
+++ src/views/entities-management/pitch/PitchManagement.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import AdminToolbar from '../../../components/navigation/AdminToolbar'
-
+import PitchManagementToolbar from '../../../components/navigation/PitchManagamentToolbar'
 // external toolbox imports 
 import { Box } from "@chakra-ui/react"
 
@@ -9,6 +9,7 @@ const PitchManagement = () => {
   return (
     <Box bg="#080808" h="100vh">
       <AdminToolbar />
+      <PitchManagementToolbar/>
     </Box>
   )
 }
