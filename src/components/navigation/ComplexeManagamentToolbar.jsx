039fb3dfba03407b2d2d4f402b506c5af7fa6ca1
Yassine Mestaoui <yassinemestaoui98@gmail.com>
2022-12-26 09:15:47 +0100

    Feat: added complexe management view

---
+++ src/components/navigation/ComplexeManagamentToolbar.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+
+// chakra UI api
+import {
+  Grid,
+  Flex,
+  Center,
+  Heading,
+  Input,
+  Button,
+  Box,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+// icons import
+import {
+  FaSearch,
+  FaPlusCircle,
+  FaLayerGroup,
+  FaFolder,
+  FaRegFutbol,
+} from "react-icons/fa";
+
+const ComplexeManagamentToolbar = () => {
+  return (
+    <Box bg="#101010" padding="1% 2%">
+      <Flex alignItems="center" justifyContent="space-evenly" width="50%">
+        <FaRegFutbol color="teal" fontSize="2rem" />
+        <Heading as="h1" size="lg" color="teal">
+          DISCOVER THE AVAILABLE COMPLEXES
+        </Heading>
+      </Flex>
+
+      <Grid
+        templateColumns="repeat(2, 1fr)"
+        gap={6}
+        width="100%"
+        margin="2% 0 1%"
+        padding="0"
+      >
+        <Flex>
+          <Center>
+            <form action="">
+              <Flex justifyContent="space-around">
+                <Input
+                  focusBorderColor="lime"
+                  placeholder="Look for a complexe"
+                  width="100%"
+                />
+                <Button
+                  leftIcon={<FaSearch />}
+                  colorScheme="green"
+                  variant="solid"
+                >
+                  Search
+                </Button>
+              </Flex>
+            </form>
+          </Center>
+        </Flex>
+        <Flex alignItems="center" justifyContent="space-evenly">
+          <Link to="/complexe-creation-form">
+            <Box
+              as="button"
+              fontSize="15px"
+              padding="5%"
+              borderRadius="2%"
+              width="10rem"
+              transition="0.5s"
+              color="#F5F5F5"
+              bg="#0FCD39"
+              _hover={{ bg: "#009420" }}
+            >
+              <Flex alignItems="center" justifyContent="space-evenly">
+                <FaPlusCircle />
+                New complexe
+              </Flex>
+            </Box>
+          </Link>
+
+          <Link to="/complexe-management">
+            <Box
+              as="button"
+              fontSize="15px"
+              padding="5%"
+              borderRadius="2%"
+              width="10rem"
+              transition="0.5s"
+              color="#F5F5F5"
+              bg={`linear-gradient(to right, #00C6FB, #005BEA)`}
+              _hover={{ bg: "#005BEA", transition: "0.5s" }}
+            >
+              <Flex alignItems="center" justifyContent="space-evenly">
+                <FaLayerGroup />
+                View all complexes
+              </Flex>
+            </Box>
+          </Link>
+
+          <Link to="/my-complexes">
+            <Box
+              as="button"
+              fontSize="15px"
+              padding="5%"
+              borderRadius="2%"
+              width="10rem"
+              transition="0.5s"
+              color="#F5F5F5"
+              bg="#00BBFF"
+              _hover={{ bg: "#00A1DB" }}
+            >
+              <Flex alignItems="center" justifyContent="space-evenly">
+                <FaFolder />
+                My complexes
+              </Flex>
+            </Box>
+          </Link>
+        </Flex>
+      </Grid>
+    </Box>
+  );
+};
+
+export default ComplexeManagamentToolbar;
---
+++ src/views/entities-management/complexe/ComplexeManagement.jsx
@@ -0,0 +1,174 @@
+import React from "react";
+
+import AdminToolbar from "../../../components/navigation/AdminToolbar";
+import ComplexeManagementToolbar from "../../../components/navigation/ComplexeManagamentToolbar";
+import ComplexeCardsNavigation from '../../../components/navigation/ComplexeCardsNavigation'
+
+// external toolbox imports
+import {
+  Box,
+  Grid,
+  Flex,
+  Center,
+  Card,
+  CardBody,
+  CardFooter,
+  ButtonGroup,
+  Button,
+  Divider,
+  Stack,
+  Image,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+
+
+// react icons import
+import { FaMapMarkerAlt } from "react-icons/fa";
+
+//assets imports
+import TestPitch from '../../../assets/img/pitch_test.jpg'
+
+const ComplexeManagement = () => {
+  return (
+    <Box bg="#080808" h="100%">
+      <AdminToolbar />
+      <ComplexeManagementToolbar />
+      <Grid templateColumns="repeat(3, 1fr)" gap={6} margin="0 1%" padding="1%">
+        <Flex>
+          <Center p="0 0.5%">
+            <Card maxW="sm" bg="#101010">
+              <CardBody>
+                <Image
+                  src={TestPitch}
+                  alt="Green double couch with wooden legs"
+                  borderRadius="lg"
+                />
+                <Stack mt="6" spacing="3">
+                  <Heading size="md" color="#F5F5F5">Northern Complexe</Heading>
+                  <Flex alignItems="center">
+                    <FaMapMarkerAlt color="#F5F5F5"/>
+                    <Text color="#F5F5F5">Marrakech, Morocco</Text>
+                  </Flex>
+                  <Text color="blue.600" fontSize="2xl">
+                    6 Pitches
+                  </Text>
+                </Stack>
+              </CardBody>
+              <Divider />
+              <CardFooter>
+                <ButtonGroup spacing="2">
+                  <Button variant="solid" colorScheme="blue">
+                    Explore pitches
+                  </Button>
+                  <Button variant="ghost" colorScheme="blue">
+                    View info
+                  </Button>
+                </ButtonGroup>
+              </CardFooter>
+            </Card>
+          </Center>
+          <Center p="0 0.5%">
+            <Card maxW="sm" bg="#101010">
+              <CardBody>
+                <Image
+                  src={TestPitch}
+                  alt="Green double couch with wooden legs"
+                  borderRadius="lg"
+                />
+                <Stack mt="6" spacing="3">
+                  <Heading size="md" color="#F5F5F5">Northern Complexe</Heading>
+                  <Flex alignItems="center">
+                    <FaMapMarkerAlt color="#F5F5F5"/>
+                    <Text color="#F5F5F5">Marrakech, Morocco</Text>
+                  </Flex>
+                  <Text color="blue.600" fontSize="2xl">
+                    6 Pitches
+                  </Text>
+                </Stack>
+              </CardBody>
+              <Divider />
+              <CardFooter>
+                <ButtonGroup spacing="2">
+                  <Button variant="solid" colorScheme="blue">
+                    Explore pitches
+                  </Button>
+                  <Button variant="ghost" colorScheme="blue">
+                    View info
+                  </Button>
+                </ButtonGroup>
+              </CardFooter>
+            </Card>
+          </Center>
+          <Center p="0 0.5%">
+            <Card maxW="sm" bg="#101010">
+              <CardBody>
+                <Image
+                  src={TestPitch}
+                  alt="Green double couch with wooden legs"
+                  borderRadius="lg"
+                />
+                <Stack mt="6" spacing="3">
+                  <Heading size="md" color="#F5F5F5">Northern Complexe</Heading>
+                  <Flex alignItems="center">
+                    <FaMapMarkerAlt color="#F5F5F5"/>
+                    <Text color="#F5F5F5">Marrakech, Morocco</Text>
+                  </Flex>
+                  <Text color="blue.600" fontSize="2xl">
+                    6 Pitches
+                  </Text>
+                </Stack>
+              </CardBody>
+              <Divider />
+              <CardFooter>
+                <ButtonGroup spacing="2">
+                  <Button variant="solid" colorScheme="blue">
+                    Explore pitches
+                  </Button>
+                  <Button variant="ghost" colorScheme="blue">
+                    View info
+                  </Button>
+                </ButtonGroup>
+              </CardFooter>
+            </Card>
+          </Center>
+          <Center p="0 0.5%">
+            <Card maxW="sm" bg="#101010">
+              <CardBody>
+                <Image
+                  src={TestPitch}
+                  alt="Green double couch with wooden legs"
+                  borderRadius="lg"
+                />
+                <Stack mt="6" spacing="3">
+                  <Heading size="md" color="#F5F5F5">Northern Complexe</Heading>
+                  <Flex alignItems="center">
+                    <FaMapMarkerAlt color="#F5F5F5"/>
+                    <Text color="#F5F5F5">Marrakech, Morocco</Text>
+                  </Flex>
+                  <Text color="blue.600" fontSize="2xl">
+                    6 Pitches
+                  </Text>
+                </Stack>
+              </CardBody>
+              <Divider />
+              <CardFooter>
+                <ButtonGroup spacing="2">
+                  <Button variant="solid" colorScheme="blue">
+                    Explore pitches
+                  </Button>
+                  <Button variant="ghost" colorScheme="blue">
+                    View info
+                  </Button>
+                </ButtonGroup>
+              </CardFooter>
+            </Card>
+          </Center>
+        </Flex>
+      </Grid>
+      <ComplexeCardsNavigation/>
+    </Box>
+  );
+};
+
+export default ComplexeManagement;
