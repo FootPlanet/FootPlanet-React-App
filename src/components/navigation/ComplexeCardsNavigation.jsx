47fa61ee737ae5361812a9bc82f68a71068f1321
Yassine Mestaoui <yassinemestaoui98@gmail.com>
2023-01-01 18:32:26 +0100

    added previous and next functionnality

--- src/components/navigation/ComplexeCardsNavigation.jsx
+++ src/components/navigation/ComplexeCardsNavigation.jsx
@@ -6,15 +6,31 @@ import { Flex, ButtonGroup, Button, Heading, Text } from "@chakra-ui/react";
 //react icons imports
 import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 
-const ComplexeCardsNavigation = () => {
+const ComplexeCardsNavigation = ({start, end, length}) => {
+
+  const previousHandler = () => {
+    if(start[0] !== 0) {
+      start[1](start[0] - 4);
+      end[1](end[0] - 4)
+
+    }
+  }
+
+  const nextHandler = () => {
+    if(end[0] <= length) {
+      start[1](start[0] + 4)
+      end[1](end[0] + 4)
+    }
+  }
+
   return (
     <Flex justifyContent="flex-end" p="1%">
       <ButtonGroup>
-        <Button colorScheme="teal" variant="outline" alignItems="center" justifyContent="space-evenly" width="8rem">
+        <Button colorScheme="teal" variant="outline" alignItems="center" justifyContent="space-evenly" width="8rem" onClick={previousHandler} >
         <FaArrowCircleLeft/>
           <Text>Previous</Text>
         </Button>
-        <Button colorScheme="teal" variant="outline" alignItems="center" justifyContent="space-evenly" width="8rem">
+        <Button colorScheme="teal" variant="outline" alignItems="center" justifyContent="space-evenly" width="8rem" onClick={nextHandler} >
         <Text>Next</Text>
         <FaArrowCircleRight/>
         </Button>
