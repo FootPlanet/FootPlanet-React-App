a01ea0b7beada28651c43438d14bbd8ed5a863d5
Yassine Mestaoui <yassinemestaoui98@gmail.com>
2023-01-05 10:12:56 +0100

    reservation with payment

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <PayPalScriptProvider deferLoading={true}>
     <App />
+    </PayPalScriptProvider>
   </React.StrictMode>
 );
