f6ff2f28dda173cadf0b66efe1de5fceb885c86c
Yassine Mestaoui <yassinemestaoui98@gmail.com>
2023-01-06 16:16:01 +0100

    fix on previous commit

--- src/views/entities-management/reservation/PitchReservation.jsx
+++ src/views/entities-management/reservation/PitchReservation.jsx
@@ -59,10 +59,12 @@ const PitchReservation = () => {
   }, [navigate, userInfo]);
 
   useEffect(() => {
-    if(userInfo.userId === pitch.owner.userId) {
-      navigate("/pitch-management");
+    if(pitch !== null) {
+      if(userInfo.userId === pitch.owner.userId) {
+        navigate("/pitch-management");
+      }
     }
-  }, [navigate, pitch.owner.userId, userInfo.userId])
+  }, [navigate, pitch, pitch.owner.userId, userInfo.userId]);
 
   useEffect(() => {
     const fetchData = async () => {
